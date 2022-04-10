323b5b98453b1844f4f5b8b2988d940251b1dfa5
User <suck205@naver.com>
2022-04-10 20:59:49 +0900

    Entity Class 생성 - 2022/04/10

--- src/main/java/jpabook/jpashop/domain/Address.java
+++ src/main/java/jpabook/jpashop/domain/Address.java
@@ -12,4 +12,14 @@ public class Address {
     private String street;
     private String zipcode;
 
+    public Address(String city, String street, String zipcode){
+        this.city = city;
+        this.street = street;
+        this.zipcode = zipcode;
+    }
+
+    protected Address(){
+
+    }
+
 }
